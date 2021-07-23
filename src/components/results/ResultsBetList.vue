78fd15033522a19bc21372c88a2db57b33d068d5
Lucas Bustos <lbustos.silva@icloud.com>
2021-07-23 15:20:13 -0300

    Corrigindo visualização da data

--- src/components/results/ResultsBetList.vue
+++ src/components/results/ResultsBetList.vue
@@ -171,6 +171,7 @@ export default {
     },
     async showDeleteModal(data) {
       this.bet = data.item;
+      console.log(parseISO(this.bet.date));
       await this.mountDeleteModal();
       this.$bvModal.show("bet-delete-modal");
     },
